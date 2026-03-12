5455e1365a6188ca5473be7fbb7a9638d8ffd79c
Alairton <alairtonlima3@gmail.com>
2026-03-12 19:29:49 -0300

    bug: ajustando vite config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(() => {
-  
-  return {
-    plugins: [react()],
-    server: {
-      port: 5173,
-      proxy: {
-        '/api': {
-          target: import.meta.env.VITE_API_URL,
-          changeOrigin: true,
-        },
-      },
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173, 
+  },
 });
